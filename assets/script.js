405502583d77e7760ae029da2e86a4f7e1b78dc8
Liam Bradley <liamhbradley11@gmail.com>
2020-09-29 22:40:35 -0700

    HTTP to HTTP

--- assets/script.js
+++ assets/script.js
@@ -33,7 +33,7 @@ $("#submit").on("click", function(){
         var cardBody = $("<div>").addClass("card-body");
         var city = $("<h4>").addClass("card-title").text(data.name);
         const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString("en-US"));
-        var image = $("<img>").attr("src",  "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        var image = $("<img>").attr("src",  "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
         var temperature = $("<p>").addClass("card-text current-temp").text(" Temperature: " + data.main.temp + " °F ");
         var humidity = $("<p>").addClass("card-text current-humidity").text(" Humidity: " + data.main.humidity + "%");
         var windSpeed = $("<p>").addClass("card-text current-wind-speed").text(" Wind Speed : " + data.wind.speed + "MPH");
@@ -76,7 +76,7 @@ $("#submit").on("click", function(){
                     var cityDate = $("<h4>").addClass("card-title").text(moment(data.list[i].dt, "X").format("MMM Do"));
                     var temperature = $("<p>").addClass("card-text forecastTemp").text("Temp: " + data.list[i].main.temp + "°F ");
                     var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + data.list[i].main.humidity + "%");
-                    var image = $("<img>").attr("src","https://openweathermap.org/img/w/" + data.list[i].weather[0].icon +".png");
+                    var image = $("<img>").attr("src","http://openweathermap.org/img/w/" + data.list[i].weather[0].icon +".png");
     
                     //append to the page
                     cardBody.append(cityDate, image, temperature, humidity);
